323e236171a06401b310e63f28caeb87f05a750c
Joanderson <joanderson.rikudo@yahoo.com>
2020-04-15 19:28:09 -0300

    FlickrBrowserApp UI completed

---
+++ FlickrBrowser/app/src/main/java/joandersongoncalves/example/flickrbrowser/SearchActivity.kt
@@ -0,0 +1,20 @@
+package joandersongoncalves.example.flickrbrowser
+
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : BaseActivity() {
+
+    private val TAG = "SearchActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, ".onCreate starts")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        activateToolbar(true)
+        Log.d(TAG, ".onCreate ends")
+
+    }
+
+}
